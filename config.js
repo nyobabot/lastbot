484764de533f3d7ba148d003f29535fd1f821fc2
MG-Botz <113174711+MgBotz@users.noreply.github.com>
2022-09-23 15:41:14 +0700

    config.js

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ global.lolkey = '8e66d0934cf741bfd2182c16' // PUNYA WAHID 357d3c85079c4e0818731e
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282171432358', 'WH MODS DEV', true],
+  ['6282171432358', 'MGBOTZ BY ALEX', true],
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true],
   ['-', 'isi_kalau_ada', true] //Ganti jd nomormu sama Namamu
