5f3839ba7612bb610dba16245f31bc67e2370efd
nyobabot <97266522+nyobabot@users.noreply.github.com>
2022-09-26 18:38:07 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -4,21 +4,15 @@ let krtu = `Kartu Intro`
 m.reply(`
 0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
 │       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
-|  *Status     :* 
+│ *Nama     : Mahir Arriyadl* 
+│ *Gender   : Male* 
+│ *Umur      : 20* 
+│ *Hobby    : Turu* 
+│ *Asal         : Jepara* 
+│ *Agama    : Islam* 
+| *Waifu     : Zero Two* 
 ╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷ཻུ۪۪ꦽꦼ̷
 ⸙
-*_BANTU RAMEKAN GROUP INI JUGA KAK_*
-https://chat.whatsapp.com/E1TElUfSJY63Cs9WqNjEEC
-*_SUBSCRIBE YT SAYA KAK :D_*
-https://youtube.com/channel/UCbI74T0AXUI26VC88NyoWdw
-`.trim()) // Tambah sendiri kalo mau
 }
 handler.command = /^(intro)$/i
 
@@ -30,27 +24,22 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
 let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•══════════════♡᭄
 │       *「 Kartu Intro 」*
-│ *Nama     :* 
-│ *Gender   :* 
-│ *Umur      :* 
-│ *Hobby    :* 
-│ *Kelas      :* 
-│ *Asal         :* 
-│ *Agama    :* 
-|  *Status     :* 
+│ *Nama     : Mahir* 
+│ *Gender   : Male* 
+│ *Umur      : 20* 
+│ *Hobby    : Turu* 
+│ *Asal         : Jepara* 
+│ *Agama    : Islam* 
+| *Waifu     : Zero Two* 
 ╰═════ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙
-*_BANTU RAMEKAN GROUP INI JUGA KAK_*
-https://chat.whatsapp.com/E1TElUfSJY63Cs9WqNjEEC
-*_SUBSCRIBE YT SAYA KAK :D_*
-https://youtube.com/channel/UCbI74T0AXUI26VC88NyoWdw
 `
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/al_pakel",
+    mediaUrl: "https://Instagram.com/mhrarrydl",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'MGBOTZ BY ALEX',
+    title: 'Mahir Bot',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
