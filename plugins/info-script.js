48d75cc738ea85c0a7a21fe18a79319c4d831b2d
nyobabot <97266522+nyobabot@users.noreply.github.com>
2022-09-26 18:41:02 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,32 +2,13 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*SUBSCRIBE*
-*https://youtube.com/channel/UCbI74T0AXUI26VC88NyoWdw*
-
-*https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow*
-
-_*JOIN GRUP KAWAN*_
-https://chat.whatsapp.com/E1TElUfSJY63Cs9WqNjEEC
-
-Pengen mentahan nya ?
-Nih ada santai bro....
-https://github.com/ImYanXiao/Elaina-MultiDevice
-
-Mau Yang No Error Script Bot nya, Gas Cek Video YT :
-https://youtu.be/Pgv_GuQTPyo
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6282127487538`
+let str = `*Gaada hehe:v
+wa.me/6289635867363`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/al_pakel",
-    mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'MGBOTZ-MD MultiDevice',
+    mediaUrl: "https://Instagram.com/mhrarrydl", 
+    title: 'Mahir Bot',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
